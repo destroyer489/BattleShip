dbe5d4fb68eb25711bf690a40bbc4c2db29705d1
dominic adamson <50681780+destroyer489@users.noreply.github.com>
2019-05-20 09:22:49 -0600

    Create Main.py
    this is a basic main file to run battleship

---
+++ Main.py
@@ -0,0 +1,24 @@
+
+# Dominic Adamson
+# Put a date here
+# Programming Class Python Battleship Game
+import Class_Board
+Class_Board.main()        
+
+# Extra credit suggestions:
+#
+# -Have the game ask if you want to play again.  Do accordingly.+
+# -Keep score of wins vs. losses+
+# -Make the board size a variable for difficulty+
+# -Add some other difficulty variable, ask the user how hard+
+#  to make the game.  Do accordingly.+
+# -Tell the user if they are close to hitting the ship
+# -Make this more like real battleship, with ships of longer
+#  length that take more than one hit to sink
+# -If a guess is in a location already guess, inform the user
+#  and allow a re-try.+
+# -Make this 2 player+
+# -Make this 2 player on separate computers
+# -Add sound effects
+# -Add graphics
+# -Bring your teacher a dessert
